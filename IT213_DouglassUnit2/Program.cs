fbe9d399a5422739fe92955487a5c14f428c8b41
Dax Douglass <dax.douglass@gmail.com>
2021-07-09 16:06:20 -0400

    Add project files.

---
+++ IT213_DouglassUnit2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace IT213_DouglassUnit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*********************************************************
+
+            //****Assignment 2 Section 1
+
+            //*********************************************************
+
+
+            Console.WriteLine("Please enter a whole number: ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another whole number: ");
+            int secNum = Convert.ToInt32(Console.ReadLine());
+
+            if (firstNum == secNum)
+            {
+                Console.WriteLine(firstNum + " = " + secNum);
+            }
+            if (firstNum != secNum)
+            {
+                Console.WriteLine(firstNum + " <> " + secNum);
+            }
+            if (firstNum < secNum)
+            {
+                Console.WriteLine(firstNum + " < " + secNum);
+            }
+            if (firstNum > secNum)
+            {
+                Console.WriteLine(firstNum + " > " + secNum);
+            }
+            if (firstNum <= secNum)
+            {
+                Console.WriteLine(firstNum + " <= " + secNum);
+            }
+            if (firstNum >= secNum)
+            {
+                Console.WriteLine(firstNum + " >= " + secNum);
+            }
+
+        }
+    }
+}
